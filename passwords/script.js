eea728e5ff640929e69f1debdda94a4bafe17970
Shaik Mohammed Adil <nexus30254@gmail.com>
2026-04-10 22:08:18 +0530

    Add files via upload

---
+++ passwords/script.js
@@ -0,0 +1,26 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T",
+"U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s",
+"t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9","~","`","!","@","#","$",
+"%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let passKey1 = document.getElementById("pass1")
+let passKey2 = document.getElementById("pass2")
+
+function passwordGenarater(){
+
+  let passwordLength = 15
+  let password = ""
+
+  for(let i=0; i<passwordLength; i++){
+  let passwordKey = Math.floor(Math.random()* characters.length)
+  password += characters[passwordKey]
+  }
+  return password
+}
+function passwordrender(){
+  passKey1.textContent = passwordGenarater()
+  passKey2.textContent = passwordGenarater()
+}
+
+
